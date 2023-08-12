ea80593b4950b41eb177012b541931271ec4164a
Wenjun <474.lucas.wang@gmail.com>
2023-08-13 01:22:06 +0800

    Initial commit

---
+++ 01-JavaSE/src/00-HelloWorld/TabTest.java
@@ -0,0 +1,7 @@
+public class TabTest {
+    public static void main(String[] args) {
+        System.out.println("123456\t111");
+        System.out.println("1\t111");
+        System.out.println("12345678\t111");
+    }
+}
