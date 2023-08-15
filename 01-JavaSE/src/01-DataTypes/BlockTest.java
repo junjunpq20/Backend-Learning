d12c11ed7044d3cf0e1824459eee2a03d277c08e
Wenjun <junjunpq20@gmail.com>
2023-08-15 22:35:32 +0800

    update

---
+++ 01-JavaSE/src/01-DataTypes/BlockTest.java
@@ -0,0 +1,12 @@
+public class BlockTest {
+    static int a = 123;
+
+    public static void main(String[] args) {
+        int a = 456;
+        System.out.println(">>> a1, " + a);
+//        {
+//            int a = 222;
+//            System.out.println(">>> a2, " + a);
+//        }
+    }
+}
---
+++ 01-JavaSE/src/01-DataTypes/FloatTest.java
@@ -0,0 +1,20 @@
+public class FloatTest {
+    public static void main(String[] args) {
+        double d1 = 123;
+        System.out.println(">>> d1, " + d1);
+
+        double d2 = 3.14;
+        System.out.println(">>> d2, " + d2);
+        double d3 = 314E-2;
+        System.out.println(">>> d3, " + d3);
+
+        double d4 = 1.2222233333;
+        System.out.println(">>> d4, " + d4);
+        double d5 = 2.33333444445555566666;
+        System.out.println(">>> d5, " + d5);
+
+        double d6 = 1.2F;
+        float f1 = 1.2F;
+        System.out.println(">>> d6 === f1, " + (d6 == f1));
+    }
+}
