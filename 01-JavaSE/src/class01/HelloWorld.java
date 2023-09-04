0bc57cd0a25a4dec943f45a229e4ddd003d177f1
Wenjun <junjunpq20@gmail.com>
2023-09-04 11:49:37 +0800

    Remove 01-JavaSE

--- 01-JavaSE/src/class01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package class01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
--- 01-JavaSE/src/class01/TabTest.java
+++
@@ -1,9 +0,0 @@
-package class01;
-
-public class TabTest {
-    public static void main(String[] args) {
-        System.out.println("123456\t111");
-        System.out.println("1\t111");
-        System.out.println("12345678\t111");
-    }
-}
