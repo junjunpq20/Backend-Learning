584d2646801b2ce981c370c58335244c39c1d43c
Wenjun <474.lucas.wang@gmail.com>
2023-09-30 14:11:34 +0800

    Update

--- X0-ZCY-Algo/tixi/src/class06/Code03_HeapSort.java
+++ X0-ZCY-Algo/tixi/src/class06/Code03_HeapSort.java
@@ -3,58 +3,49 @@ package class06;
 // https://leetcode.com/problems/sort-an-array/
 public class Code03_HeapSort {
   class Solution {
-    public static void swap(int[] arr, int i, int j) {
+    public int[] sortArray(int[] nums) {
+      int N = nums.length;
+
+      // for (int i = 0; i < N; i++) {
+      //     heapInsert(nums, i);
+      // }
+
+      for (int i = N - 1; i >= 0; i--) {
+        heapify(nums, i, N);
+      }
+
+      for (int i = N - 1; i >= 0; i--) {
+        swap(nums, 0, i);
+        heapify(nums, 0, i);
+      }
+
+      return nums;
+    }
+
+    // 大根堆
+    private void heapInsert(int[] arr, int i) {
+      while (arr[i] > arr[(i - 1) / 2]) {
+        swap(arr, i, (i - 1) / 2);
+        i = (i - 1) / 2;
+      }
+    }
+
+    private void heapify(int[] arr, int i, int heapSize) {
+      while ((i * 2 + 1) < heapSize) {
+        int j = i * 2 + 1;
+        j = j + 1 < heapSize && arr[j + 1] > arr[j] ? j + 1 : j;
+        if (arr[i] >= arr[j]) break;
+        swap(arr, i, j);
+        i = j;
+      }
+    }
+
+    private void swap(int[] arr, int i, int j) {
       if (i == j) return;
 
       arr[i] = arr[i] ^ arr[j];
       arr[j] = arr[i] ^ arr[j];
       arr[i] = arr[i] ^ arr[j];
-    }
-
-    public int[] sortArray(int[] nums) {
-      heapSort(nums);
-      return nums;
-    }
-
-    private void heapSort(int[] nums) {
-      // 构建大根堆
-
-      // O(NlogN)
-      for (int i = 0; i < nums.length; i++) {
-        heapInsert(nums, i);
-      }
-
-      //      // OR
-      //      // O(N)
-      //      for (int i = nums.length - 1; i >= 0; i--) {
-      //        heapify(nums, i, nums.length);
-      //      }
-
-      int heapSize = nums.length;
-      swap(nums, 0, --heapSize);
-      while (heapSize > 0) {
-        heapify(nums, 0, heapSize);
-        swap(nums, 0, --heapSize);
-      }
-    }
-
-    // 拆入一个元素，不断往上走
-    private void heapInsert(int[] arr, int index) {
-      while (arr[index] > arr[(index - 1) / 2]) {
-        swap(arr, index, (index - 1) / 2);
-        index = (index - 1) / 2;
-      }
-    }
-
-    // 不断往下走，重塑堆结构
-    private void heapify(int[] arr, int i, int heapSize) {
-      while ((i * 2 + 1) < heapSize) {
-        int j = i * 2 + 1;
-        if ((j + 1) < heapSize && arr[j + 1] > arr[j]) j++;
-        if (arr[i] >= arr[j]) break;
-        swap(arr, i, j);
-        i = j;
-      }
     }
   }
 }
